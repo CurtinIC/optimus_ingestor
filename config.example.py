2e4fea244a8cb4fb337fec3bb85fcc528ffc2bb2
Simon Huband <simon.huband@curtin.edu.au>
2016-10-24 14:34:45 +0800

    AN-830 #comment removing unused example config properties

--- config.example.py
+++ config.example.py
@@ -4,9 +4,6 @@ SQL_USERNAME = 'root'
 SQL_PASSWORD = ''
 #Mongo Server details
 MONGO_HOST = 'localhost'
-#Cache details - whether to call a URL once an ingestion script is finished
-RESET_CACHE = False
-RESET_CACHE_URL = 'http://example.com/visualization_reload/'
 #Fab - configuration for deploying to a remote server
 FAB_HOSTS = []
 FAB_GITHUB_URL = 'https://github.com/UQ-UQx/optimus_ingestor.git'
@@ -14,7 +11,6 @@ FAB_REMOTE_PATH = '/file/to/your/deployment/location'
 #Ignored services
 IGNORE_SERVICES = ['extract_sample', 'eventcount', 'daily_count']
 #File output
-OUTPUT_DIRECTORY = '/tmp'
 DATA_PATHS = ['/data/']
 EXPORT_PATH = '/Volumes/VMs/export'
 MONGO_PATH = '/usr/local/bin/'
