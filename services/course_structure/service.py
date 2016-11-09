32263b6e5589c3b7c0ea4aa6408165d3e03a748f
Simon Huband <simon.huband@curtin.edu.au>
2016-11-09 08:59:11 +0800

    AN-830 #comment aligning hyphenation to be consistent for course structure json file names

--- services/course_structure/service.py
+++ services/course_structure/service.py
@@ -72,8 +72,9 @@ class CourseStructure(base_service.BaseService):
                     policyfile = open(policyfileurl).read()
                     policydata = json.loads(policyfile)
                     course['policy'] = policydata
-                    f = open(self.outputdir + '/' + coursename + '.json', 'w+')
-                    print self.outputdir + '/' + coursename + '.json'
+                    json_file = coursename.replace("_", "-") + '.json'
+                    f = open(self.outputdir + '/' + json_file, 'w+')
+                    print self.outputdir + '/' + json_file
                     f.write(json.dumps(course))
                 except IOError as e:
                     print "COURSE STRUCTURE FILE DOES NOT EXIST " + str(coursefile)
